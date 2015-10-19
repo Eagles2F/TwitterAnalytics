796ad1ad36de75af60a97abee90dab2b2573af8e
Evan <thueeliyifan@gmail.com>
2015-10-18 20:34:27 -0700

    add decipher

--- server.go
+++ server.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"math/big"
+	"math"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"unicode/utf8"
 	"os"
 	"time"
+	"bytes"
 )
 
 type Config struct {
@@ -24,7 +28,6 @@ func initConfig() {
 	if err != nil {
 		fmt.Println("error:", err)
 	}
-	fmt.Println(config)
 }
 
 func q1Handler(w http.ResponseWriter, r *http.Request) {
@@ -34,12 +37,51 @@ func q1Handler(w http.ResponseWriter, r *http.Request) {
 	// Time
 	now := time.Now()
 	response := fmt.Sprintf("%s,%s\n%s\n%s", config.TeamId, config.TeamAwsAccountId,
-		now.Format("2006-01-02 15:04:05"), decipher(key, message))
+		now.Format("2006-01-02 15:04:05"), decipher(message, key))
 	io.WriteString(w, response)
 }
 
-func decipher(product string, cipher string) string {
-	return ""
+func decipher(message string, key string) string {
+	//calculate key Y
+    sk := big.NewInt(0)
+    sk.SetString(config.SecretKey, 10)
+    k := big.NewInt(0)
+    k.SetString(key, 10)
+    var y big.Int;
+    y.Div(k, sk)
+    n := int(math.Sqrt(float64(utf8.RuneCountInString(message))))
+
+    //de-diagonalize
+    var buffer bytes.Buffer;
+
+    for i := 0; i < 2*n - 1; i++ {
+        var z int
+        if i < n {
+            z = 0;
+        } else {
+            z = i - n + 1
+        }
+        for j := z; j <= i - z; j++ {
+            buffer.WriteString(string(message[j*n + i - j]))
+        }
+    }
+
+    intermediate := buffer.String()
+
+    //shift back
+    yn:= y.Int64()
+    z := yn % 25 + 1
+    zint := int(z)
+    var buffer1 bytes.Buffer;
+    for i := 0; i < len(intermediate); i++ {
+        order := int(intermediate[i] - 'A')
+        if order < zint {
+            buffer1.WriteString(string('Z' - (zint - order - 1)))
+        } else {
+            buffer1.WriteString(string(int(intermediate[i]) - zint))
+        }
+    }
+    return buffer1.String()
 }
 
 func main() {
