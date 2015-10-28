9cd6a8067becea060edc7f999ab094e51d158404
Evan <thueeliyifan@gmail.com>
2015-10-27 22:21:49 -0700

    add instaces

--- tweet.sh
+++ tweet.sh
@@ -3,4 +3,4 @@
 
 cd /home/ubuntu/TwitterAnalytics/
 
-sudo env PATH="$PATH" CLASSPATH="$CLASSPATH" vertx run Server.java -instances 10
+sudo env PATH="$PATH" CLASSPATH="$CLASSPATH" vertx run Server.java -instances 20
