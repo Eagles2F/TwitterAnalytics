2480312eeb65ca482933fb22bad8b16784c85642
Evan <thueeliyifan@gmail.com>
2015-10-27 22:41:13 -0700

    30 ins

--- tweet.sh
+++ tweet.sh
@@ -3,4 +3,4 @@
 
 cd /home/ubuntu/TwitterAnalytics/
 
-sudo env PATH="$PATH" CLASSPATH="$CLASSPATH" vertx run Server.java -instances 20
+sudo env PATH="$PATH" CLASSPATH="$CLASSPATH" vertx run Server.java -instances 30
