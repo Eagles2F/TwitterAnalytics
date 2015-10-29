473e5dbf728d0b6ef18ba8a18daf77099ed3cd3f
chuangxie <chuangxie0111@gmail.com>
2015-10-29 16:23:43 -0700

    tweets sql

--- create_tweets.sql
+++ create_tweets.sql
@@ -1,7 +1,7 @@
 create table tweets(
   tweetId CHAR(20) NOT NULL, 
   idtime CHAR(90) NOT NULL, 
-  text CHAR(145) NOT NULL DEFAULT '', 
+  text VARCHAR(300) NOT NULL DEFAULT '', 
   score int NOT NULL) ENGINE=MyISAM DEFAULT CHARACTER SET=utf8mb4;
 
 alter table tweets add index (idtime);
--- import_tweets.sql
+++
@@ -1,6 +0,0 @@
-insert into tweets (tweetId, userId, time, text, score) VALUES (
-  "8234234", 
-  "12313424", 
-  "2012-10-05+10:05:30", 
-  "this is a fake \tweet", 
-  10);
