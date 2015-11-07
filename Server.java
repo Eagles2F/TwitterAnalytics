c1422957dec94063d0ff900f0a46432e923e7ae0
Evan <thueeliyifan@gmail.com>
2015-11-07 02:12:47 -0800

    fix

--- Server.java
+++ Server.java
@@ -226,11 +226,13 @@ public class Server extends Verticle {
             if (negList.size() > 0) {
                 neg.append("Negative Tweets\n");
             }
-            for (Tweet tw : posList) {
-                pos.append(String.format("%s,%s,%s,%s\n",tw.date,tw.score,tw.id,tw.text));
+            for (int i=0;i<posList.size();i++) {
+                pos.append(String.format("%s,%s,%s,%s\n",posList.get(i).
+                date,posList.get(i).score,posList.get(i).id,posList.get(i).text));
             }
-                        for (Tweet tw : negList) {
-                neg.append(String.format("%s,%s,%s,%s\n",tw.date,tw.score,tw.id,tw.text));
+            for (int i=0;i<negList.size();i++) {
+                neg.append(String.format("%s,%s,%s,%s\n",negList.get(i).
+                date,negList.get(i).score,negList.get(i).id,negList.get(i).text));
             }
 
             if (posList.size() > 0 && negList.size() >0) {
